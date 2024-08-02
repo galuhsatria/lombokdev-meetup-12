4d8fef2febd741785db9de081f187cafd3b69f8b
galuhsatria <galuhsatria219@gmail.com>
2024-08-02 21:22:44 +0800

    adjust style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { IoIosArrowForward } from 'react-icons/io';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -8,13 +10,15 @@ export default function Home() {
         <Image src={'/images/background.png'} alt="bg-image" width={800} height={100} />
       </div>
 
-      <section className="hero h-screen">
-        <div className="text-center">
-          <h1 className="text-accent md:text-5xl sm:text-4xl text-3xl font-extrabold">Lombok Dev Meetup #12</h1>
+      <section className="hero h-screen flex items-center justify-center">
+        <div className="flex items-center  text-center flex-col ">
+          <h1 className="text-accent md:text-6xl sm:text-4xl text-3xl font-extrabold">Lombok Dev Meetup #12</h1>
           <p className="text-white max-w-2xl mt-2  md:text-base sm:text-sm text-xs">Belajar teknologi terbaru, networking, dan dapetin insight seru dari para speaker kece. Don&apos;t miss out!</p>
-          <button className="btn bg-accent border-none text-white mt-6">
-            Daftar Sekarang <IoIosArrowForward />
-          </button>
+          <Button className="bg-accent cursor-pointer mt-6">
+            <Link href={'/'} className="flex items-center gap-3">
+              Daftar Sekarang <IoIosArrowForward />
+            </Link>
+          </Button>
         </div>
       </section>
     </main>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,67 +4,88 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { IoSunnyOutline, IoMenu } from 'react-icons/io5';
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="fixed w-full z-30">
-      <nav className="px-4 py-4 max-w-6xl mx-auto flex items-center justify-between max-md:bg-secondary">
+    <header className="fixed w-full z-30  max-md:bg-black/30 backdrop-blur-md">
+      <nav className="p-4 max-md:py-3 max-w-6xl mx-auto flex items-center justify-between ">
         <div className="logo">
-          <Image src={'/images/logo.png'} alt="Logo Lombok Dev Meetup" width={50} height={100} />
+          <Image
+            src={'/images/logo.png'}
+            alt="Logo Lombok Dev Meetup"
+            width={70}
+            height={100}
+            className="
+          max-md:w-14"
+          />
         </div>
 
-        <div
-          className={`menu max-md:absolute max-md:top-20 max-md:bg-secondary max-md:border-t max-md:border-[#1A1E26] left-0 backdrop-blur-md transition-all duration-200 max-md:w-full max-md:h-screen ${
-            isOpen ? 'max-md:block' : 'max-md:opacity-0 max-md:hidden '
-          }`}
-        >
-          <ul className="flex gap-4 max-md:flex-col max-md:items-center max-md:pt-6">
+        <div className={`menu transition-all duration-200 max-md:hidden`}>
+          <ul className="flex gap-10">
             <li>
-              <Link href="#tentang" className="no-underline text-white cursor-pointer">
+              <Link href="#tentang" className="no-underline  hover:text-accent transition-all duration-200 text-white cursor-pointer">
                 Tentang
               </Link>
             </li>
             <li>
-              <Link href="#tentang" className="no-underline text-white cursor-pointer">
+              <Link href="#tentang" className="no-underline text-white cursor-pointer hover:text-accent transition-all duration-200 ">
                 Sesi
               </Link>
             </li>
             <li>
-              <Link href="#tentang" className="no-underline text-white cursor-pointer">
+              <Link href="#tentang" className="no-underline text-white cursor-pointer hover:text-accent transition-all duration-200 ">
                 Pembicara
               </Link>
             </li>
             <li>
-              <Link href="#tentang" className="no-underline text-white cursor-pointer">
+              <Link href="#tentang" className="no-underline text-white cursor-pointer hover:text-accent transition-all duration-200 ">
                 Hadiah
               </Link>
             </li>
             <li>
-              <Link href="#tentang" className="no-underline text-white cursor-pointer">
+              <Link href="#tentang" className="no-underline text-white cursor-pointer hover:text-accent transition-all duration-200 ">
                 FAQ
               </Link>
-            </li>
-            <li className="max-md:hidden">
-              <button>
-                <IoSunnyOutline className="text-xl" />
-              </button>
             </li>
           </ul>
         </div>
 
-        <div className="hidden max-md:flex items-center gap-6">
-          <div>
-            <button>
-              <IoSunnyOutline className="text-xl" />
-            </button>
-          </div>
-          <div>
-            <button onClick={() => setIsOpen(!isOpen)}>
-              <IoMenu className="text-2xl" />
-            </button>
-          </div>
-        </div>
+        <Sheet className="bg-[#1A1E26] text-white hidden max-md:block">
+          <SheetTrigger className='max-md:block hidden'>
+            <IoMenu className="text-2xl text-white" />
+          </SheetTrigger>
+          <SheetContent className="bg-[#1A1E26] border-none">
+            <ul className="flex gap-6 flex-col mt-10">
+              <li>
+                <Link href="#tentang" className="no-underline  hover:text-accent transition-all duration-200 text-white cursor-pointer">
+                  Tentang
+                </Link>
+              </li>
+              <li>
+                <Link href="#tentang" className="no-underline text-white cursor-pointer hover:text-accent transition-all duration-200 ">
+                  Sesi
+                </Link>
+              </li>
+              <li>
+                <Link href="#tentang" className="no-underline text-white cursor-pointer hover:text-accent transition-all duration-200 ">
+                  Pembicara
+                </Link>
+              </li>
+              <li>
+                <Link href="#tentang" className="no-underline text-white cursor-pointer hover:text-accent transition-all duration-200 ">
+                  Hadiah
+                </Link>
+              </li>
+              <li>
+                <Link href="#tentang" className="no-underline text-white cursor-pointer hover:text-accent transition-all duration-200 ">
+                  FAQ
+                </Link>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
       </nav>
     </header>
   );
